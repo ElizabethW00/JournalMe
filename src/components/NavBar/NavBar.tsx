6b569c58fcc3fe790450dd0dad10cbcea6e96dfe
Elizabeth W <71103650+ElizabethW00@users.noreply.github.com>
2025-04-02 14:23:16 -0400

    styling home

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,24 +5,24 @@ import Scribble from "../Scribble";
 
 const NavBar = () => {
   return (
-    <nav className="topNav">
-      <div className="logo">
-        <div>JM</div>
-      </div>
-
-      <div className="links">
-        <div className="hyperlink">
-          <p>About</p>
-          <Scribble
-            regular
-            className="text-[20px] w-full h-full"
-            innerClassName="top-14"
-            text={"about"}
-            animate
-          />
+    <nav className="absolute top-0 w-screen">
+      <div className="topNav">
+        <div className="logo">
+          <div>JM</div>
         </div>
-
-        <div className="signInButton">Sign In</div>
+        <div className="links">
+          <div className="hyperlink">
+            <p>About</p>
+            <Scribble
+              regular
+              className="text-[20px] w-full h-full"
+              innerClassName="top-14"
+              text={"about"}
+              animate
+            />
+          </div>
+          <div className="signInButton">Sign In</div>
+        </div>
       </div>
     </nav>
   );
